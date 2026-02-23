3b191c47b9615d382a43c4440e0053dfcb357c39
ikernavarrro <iker.navarro.perez@zabalburu.org>
2026-02-23 20:20:33 +0100

    Añadida vista GestockVista en proyecto Gestock, diseñado panel almacenes, falta añadir funciones.

--- # Refuerzo/#### Proyecto_GESTOCK/Gestock/src/main/java/org/zabalburu/daw1/gestock/Gestock.java
+++ # Refuerzo/#### Proyecto_GESTOCK/Gestock/src/main/java/org/zabalburu/daw1/gestock/Gestock.java
@@ -5,7 +5,6 @@
 package org.zabalburu.daw1.gestock;
 import com.formdev.flatlaf.themes.FlatMacDarkLaf;
 import org.zabalburu.daw1.gestock.util.ConexionBBDD;
-import org.zabalburu.daw1.gestock.vista.GestockVistaMain;
 
 /**
  *
@@ -15,8 +14,6 @@ public class Gestock {
 
     public static void main(String[] args) {
         FlatMacDarkLaf.setup();
-        GestockVistaMain pruebas = new GestockVistaMain();
-        pruebas.setVisible(true);
         System.out.println("Hello Gestock!");
         System.out.println(ConexionBBDD.getConnection());
     }
