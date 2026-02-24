5cba2b835cb7d5861263d1afc6ba238057a0e1a5
ikernavarrro <iker.navarro.perez@zabalburu.org>
2026-02-24 10:06:49 +0100

    Añadido paquete vista a AplicacionTorneo, terminado diseño del PnlDashboard

--- 3er-Trimestre/AplicacionTorneo/src/main/java/org/zabalburu/daw1/aplicaciontorneo/AplicacionTorneo.java
+++ 3er-Trimestre/AplicacionTorneo/src/main/java/org/zabalburu/daw1/aplicaciontorneo/AplicacionTorneo.java
@@ -1,8 +1,16 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.zabalburu.daw1.aplicaciontorneo;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+import java.awt.Dimension;
+import java.awt.Insets;
+import javax.swing.BorderFactory;
+import javax.swing.UIManager;
+import org.zabalburu.daw1.aplicaciontorneo.vista.FrmTorneo;
 
 /**
  *
@@ -12,5 +20,34 @@ public class AplicacionTorneo {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        FlatLaf.setup(new FlatMacLightLaf());
+        // Redondeo general
+        UIManager.put("Component.arc", 12);
+        UIManager.put("Button.arc", 25);
+        UIManager.put("TextComponent.arc", 10);
+        UIManager.put("ScrollBar.thumbArc", 999);
+
+        // Espaciado (que no se vea "apretado")
+        UIManager.put("TextField.margin", new Insets(6, 10, 6, 10));
+        UIManager.put("PasswordField.margin", new Insets(6, 10, 6, 10));
+        UIManager.put("TextArea.margin", new Insets(8, 10, 8, 10));
+        UIManager.put("Button.margin", new Insets(8, 14, 8, 14));
+
+        // Focus más discreto
+        UIManager.put("Component.focusWidth", 2);
+        UIManager.put("Component.innerFocusWidth", 0);
+
+        // Tablas más modernas
+        UIManager.put("Table.rowHeight", 30);
+        UIManager.put("Table.showHorizontalLines", false);
+        UIManager.put("Table.showVerticalLines", false);
+        UIManager.put("Table.intercellSpacing", new Dimension(0, 0));
+
+        // Scrollbars finas
+        UIManager.put("ScrollBar.width", 12);
+
+        // (Opcional) Quitar borde por defecto de ScrollPane
+        UIManager.put("ScrollPane.border", BorderFactory.createEmptyBorder());
+        new FrmTorneo().setVisible(true);
     }
 }
