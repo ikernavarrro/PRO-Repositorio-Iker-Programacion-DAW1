c6a5f35e731a14788590e25b6f52788bcabadd77
Iker Navarro Pérez <iker.navarro.perez@zabalburu.org>
2026-04-08 01:55:46 +0200

    Actualizada actividad Gestion_Streamers añadida vista, terminado diseño de FrmMenu con CardLayout, añadidos PnlDashboard, PnlStreamer y PnlSesiones, pendientes de diseñar y terminar.

--- 3er-Trimestre/Gestion_Streamers/src/main/java/org/zabalburu/gestion_streamers/Gestion_Streamers.java
+++ 3er-Trimestre/Gestion_Streamers/src/main/java/org/zabalburu/gestion_streamers/Gestion_Streamers.java
@@ -1,8 +1,11 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package org.zabalburu.gestion_streamers;
+
+import com.formdev.flatlaf.FlatLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import org.zabalburu.gestion_streamers.view.FrmMenu;
 
 /**
  *
@@ -11,6 +14,7 @@ package org.zabalburu.gestion_streamers;
 public class Gestion_Streamers {
 
     public static void main(String[] args) {
-        
+        FlatLaf.setup(new FlatMacDarkLaf());
+        new FrmMenu().setVisible(true);
     }
 }
